da5dd292cd8d3ff1ae62021b9e72d9c8eca5c87a
HPTrongMC <120317993+HPTrongMC@users.noreply.github.com>
2024-07-15 08:09:37 +0700

    Update Gitpod.sh

--- Gitpod.sh
+++ Gitpod.sh
@@ -1,6 +1,7 @@
 curl -L -o ideaIC.tar.gz "https://download-cdn.jetbrains.com/idea/ideaIC-2024.1.4.tar.gz?_gl=1*68fjb*_gcl_au*MTY4MjI5MDM0OS4xNzIwNzg0NzY3*_ga*MTIxMzA3MzcwMy4xNzIwNzg0NzQ4*_ga_9J976DJZ68*MTcyMTAwNDgyNy42LjAuMTcyMTAwNDgzMi41NS4wLjA."
-mkdir -p /intelliJ
-tar -ztvf ideaIC.tar.gz -C ./intellij
+
+gunzip ideaIC.tar.gz
+tar -ztvf ideaIC.tar
 
 curl -s "https://get.sdkman.io" | bash
 source "$HOME/.sdkman/bin/sdkman-init.sh"
