07d96055549fa1f955e83c6434746bfed2d1d258
HPTrongMC <120317993+HPTrongMC@users.noreply.github.com>
2024-07-15 07:57:17 +0700

    Update Gitpod.sh

--- Gitpod.sh
+++ Gitpod.sh
@@ -1,6 +1,8 @@
-tar -ztvf ideaIC-2024.1.4.tar.gz -C ./intellij
+curl -L -o ideaIC.tar.gz "https://download-cdn.jetbrains.com/idea/ideaIC-2024.1.4.tar.gz?_gl=1*68fjb*_gcl_au*MTY4MjI5MDM0OS4xNzIwNzg0NzY3*_ga*MTIxMzA3MzcwMy4xNzIwNzg0NzQ4*_ga_9J976DJZ68*MTcyMTAwNDgyNy42LjAuMTcyMTAwNDgzMi41NS4wLjA."
+mkdir -p /intelliJ
+tar -ztvf ideaIC.tar.gz -C ./intellij
 
 curl -s "https://get.sdkman.io" | bash
 source "$HOME/.sdkman/bin/sdkman-init.sh"
 
-sdk install java 8.0.302-open -y
+sdk install java 8.0.302-open
